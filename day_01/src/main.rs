387185018417459a5b034bb066ed3b2fc07e238a
Matt Mullins <mmullins@mmlx.us>
2019-12-04 00:05:29 -0800

    day 01: also compute fuel required to carry fuel

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -5,18 +5,33 @@ fn main() {
     let lock = stdin.lock();
 
     let mut total_fuel = 0;
+    let mut total_fuel_including_fuel_for_the_fuel = 0;
 
     for line in lock.lines() {
         let line = line.expect("file was not UTF-8");
         let mass = line.parse().expect("could not parse line");
 
-        total_fuel += calculate_fuel(mass);
+        let mut new_fuel = calculate_fuel(mass);
+        total_fuel += new_fuel;
+        total_fuel_including_fuel_for_the_fuel += new_fuel;
+
+        loop {
+            new_fuel = calculate_fuel(new_fuel);
+            if new_fuel <= 0 {
+                break;
+            }
+            total_fuel_including_fuel_for_the_fuel += new_fuel;
+        }
     }
 
     println!("Total fuel: {}", total_fuel);
+    println!(
+        "Total fuel, including fuel for the fuel: {}",
+        total_fuel_including_fuel_for_the_fuel
+    );
 }
 
-fn calculate_fuel(mass: usize) -> usize {
+fn calculate_fuel(mass: isize) -> isize {
     mass / 3 - 2
 }
 
