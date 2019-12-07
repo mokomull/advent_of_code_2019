c7e27173160354b63abea5a3ff994d6eaddee876
Matt Mullins <mmullins@mmlx.us>
2019-12-07 09:33:07 -0800

    day 02: start with some failing tests

---
+++ day_02/src/main.rs
@@ -0,0 +1,26 @@
+fn parse_opcodes(input: &str) -> Vec<usize> {
+    unimplemented!()
+}
+
+fn run(opcodes: Vec<usize>) -> Vec<usize> {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn parser() {
+        assert_eq!(
+            super::parse_opcodes("1,9,10,3,2,3,11,0,99,30,40,50"),
+            vec!(1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50)
+        );
+    }
+
+    #[test]
+    fn run() {
+        assert_eq!(
+            super::run(super::parse_opcodes("1,9,10,3,2,3,11,0,99,30,40,50")),
+            vec!(30, 1, 1, 4, 2, 5, 6, 0, 99)
+        );
+    }
+}
