d84e3c975e1102a24dd73f5403886de47ed855ea
Matt Mullins <mmullins@mmlx.us>
2019-12-11 22:18:25 -0800

    day 04: implement the checks, test them

---
+++ day_04/src/main.rs
@@ -0,0 +1,48 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn check(i: usize) -> bool {
+    let stringy = format!("{}", i);
+
+    adjacent_digits(&stringy) && in_order(&stringy)
+}
+
+fn adjacent_digits(i: &str) -> bool {
+    for (i, j) in i.chars().zip(i.chars().skip(1)) {
+        if i == j {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn in_order(i: &str) -> bool {
+    let mut last = i
+        .chars()
+        .nth(0)
+        .expect("in_order check for an empty string");
+
+    for c in i.chars().skip(1) {
+        if c < last {
+            return false;
+        }
+        last = c;
+    }
+
+    true
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn check() {
+        assert!(super::check(122345));
+        assert!(super::check(111123));
+        assert!(super::in_order("135679"));
+        assert!(super::check(111111));
+        assert!(!super::check(223450));
+        assert!(!super::check(123789));
+    }
+}
