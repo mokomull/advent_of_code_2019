5dbfa90b72043d880cb26dd5fad4677f82120875
Matt Mullins <mmullins@mmlx.us>
2019-12-14 14:36:57 -0800

    day 05: get the diagnostic code from my input

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -3,7 +3,17 @@ fn main() {
 }
 
 fn do_main(path: &str) {
-    unimplemented!();
+    let program =
+        intcode::parse_opcodes(&std::fs::read_to_string(path).expect("could not read input"));
+    let input = vec![1]; // the flavortext says there is exactly one input, value 1
+
+    let (_memory, output) = intcode::run_with_io(program, input.into());
+
+    // all but the last output should be 0, as stated in the flavortext
+    assert!(output.iter().take(output.len() - 1).all(|&x| x == 0));
+    let diagnostic_code = output.last().expect("empty output");
+    println!("Diagnostic code: {}", diagnostic_code);
+    assert_eq!(*diagnostic_code, 13294380);
 }
 
 #[cfg(test)]
