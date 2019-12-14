3b0219cd02e9209d546e7460919de8fbc27567f5
Matt Mullins <mmullins@mmlx.us>
2019-12-14 14:27:51 -0800

    day 05: skeleton of main()

---
+++ day_05/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    do_main("inputs/day_05.txt");
+}
+
+fn do_main(path: &str) {
+    unimplemented!();
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn main() {
+        super::do_main("../inputs/day_05.txt");
+    }
+}
