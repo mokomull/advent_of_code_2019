c7971c16d72f7051e09d7715d5f18d6f45eaa8d3
Matt Mullins <mmullins@mmlx.us>
2019-12-14 13:43:48 -0800

    day 05: implement "parameter modes" for intcode
    The original program in day 2 did not use any of these (since it would
    have panic'ed otherwise), so this should be safe to do in common code.

--- intcode/src/lib.rs
+++ intcode/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 pub fn parse_opcodes(input: &str) -> Vec<usize> {
     input
         .trim()
@@ -6,31 +8,61 @@ pub fn parse_opcodes(input: &str) -> Vec<usize> {
         .collect()
 }
 
-pub fn run(mut opcodes: Vec<usize>) -> Vec<usize> {
+pub fn run(opcodes: Vec<usize>) -> Vec<usize> {
+    let (memory, _output) = run_with_io(opcodes, VecDeque::new());
+    memory
+}
+
+// Returns (memory, output)
+pub fn run_with_io(
+    mut opcodes: Vec<usize>,
+    mut input: VecDeque<usize>,
+) -> (Vec<usize>, Vec<usize>) {
     let mut ip = 0;
 
     loop {
-        match opcodes[ip] {
+        match opcodes[ip] % 100 {
             1 => {
-                let source1 = opcodes[ip + 1];
-                let source2 = opcodes[ip + 2];
-                let destination = opcodes[ip + 3];
-                opcodes[destination] = opcodes[source1] + opcodes[source2];
-                ip += 4;
+                let (source1, source2, destination) = get_operands_3(&opcodes, &mut ip);
+                opcodes[destination] = source1 + source2;
             }
             2 => {
-                let source1 = opcodes[ip + 1];
-                let source2 = opcodes[ip + 2];
-                let destination = opcodes[ip + 3];
-                opcodes[destination] = opcodes[source1] * opcodes[source2];
-                ip += 4;
+                let (source1, source2, destination) = get_operands_3(&opcodes, &mut ip);
+                opcodes[destination] = source1 * source2;
             }
             99 => break,
             x => panic!("unexpected opcode found in position {}: {}", ip, x),
         }
     }
 
-    opcodes
+    (opcodes, vec![])
+}
+
+fn get_operands_3(opcodes: &[usize], ip: &mut usize) -> (usize, usize, usize) {
+    let source1_idx = opcodes[*ip + 1];
+    let source2_idx = opcodes[*ip + 2];
+    let destination_idx = opcodes[*ip + 3];
+
+    let source1 = match opcodes[*ip] / 100 % 10 {
+        0 => opcodes[source1_idx],
+        1 => source1_idx,
+        x => panic!("Invalid parameter mode {} at ip {}", x, *ip),
+    };
+
+    let source2 = match opcodes[*ip] / 1000 % 10 {
+        0 => opcodes[source2_idx],
+        1 => source2_idx,
+        x => panic!("Invalid parameter mode {} at ip {}", x, *ip),
+    };
+
+    let destination = match opcodes[*ip] / 10000 % 10 {
+        0 => destination_idx,
+        x => panic!("Invalid destination parameter mode {} at ip {}", x, *ip),
+    };
+
+    *ip += 4;
+
+    (source1, source2, destination)
 }
 
 #[cfg(test)]
@@ -48,6 +80,20 @@ mod tests {
         assert_eq!(
             super::run(super::parse_opcodes("1,9,10,3,2,3,11,0,99,30,40,50")),
             vec!(3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50)
+        );
+    }
+
+    #[test]
+    fn parameter_modes() {
+        assert_eq!(
+            super::run(vec![1002, 4, 3, 4, 33]),
+            vec![
+                1002,
+                4,
+                3,
+                4,
+                33 /* memory at 4 */ * 3 /* immediate 3 */
+            ]
         );
     }
 }
