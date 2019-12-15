3ac586a779fcd03dd1c4c3657abc104786541170
Matt Mullins <mmullins@mmlx.us>
2019-12-14 16:14:36 -0800

    day 05: calculate diagnostic code for "ID 5"

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -7,13 +7,21 @@ fn do_main(path: &str) {
         intcode::parse_opcodes(&std::fs::read_to_string(path).expect("could not read input"));
     let input = vec![1]; // the flavortext says there is exactly one input, value 1
 
-    let (_memory, output) = intcode::run_with_io(program, input.into());
+    let (_memory, output) = intcode::run_with_io(program.clone(), input.into());
 
     // all but the last output should be 0, as stated in the flavortext
     assert!(output.iter().take(output.len() - 1).all(|&x| x == 0));
     let diagnostic_code = output.last().expect("empty output");
     println!("Diagnostic code: {}", diagnostic_code);
     assert_eq!(*diagnostic_code, 13294380);
+
+    let input = vec![5]; // for part 2, the flavortext says it's 5 now.
+    let (_memory, output) = intcode::run_with_io(program, input.into());
+    // and it says the program will generate exactly one output
+    assert!(output.len() == 1);
+    let diagnostic_code = output.last().unwrap();
+    println!("Diagnostic code for system ID 5: {}", diagnostic_code);
+    assert_eq!(*diagnostic_code, 11460760);
 }
 
 #[cfg(test)]
