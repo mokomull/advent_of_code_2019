d5ab010b74719ea3b7af287b91f9a84f1e20c7ec
Matt Mullins <mmullins@mmlx.us>
2019-12-21 22:37:39 -0800

    day 08: calculate # ones * # twos
    This solves part 1.

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -1,9 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    do_main(std::fs::File::open("inputs/day_08.txt").expect("could not open input"));
+}
+
+fn do_main<T: std::io::Read>(input: T) {
+    use std::io::BufRead;
+
+    let mut reader = std::io::BufReader::new(input);
+    let mut line = String::new();
+    reader
+        .read_line(&mut line)
+        .expect("could not read a line from input");
+
+    let mut layers = split_into_layers(line.trim().as_bytes(), 25, 6);
+    layers.sort_by_key(|l| l.iter().filter(|&&pixel| pixel == b'0').count());
+    let layer = layers.first().expect("somehow there were no layers");
+    let ones = layer.iter().filter(|&&pixel| pixel == b'1').count();
+    let twos = layer.iter().filter(|&&pixel| pixel == b'2').count();
+    println!("# of ones * # of twos is {}", ones * twos);
+    assert_eq!(ones * twos, 2210);
 }
 
 fn split_into_layers(pixels: &[u8], width: usize, height: usize) -> Vec<Vec<u8>> {
-    assert!(pixels.len() % (width * height) == 0);
+    assert_eq!(pixels.len() % (width * height), 0);
     pixels
         .chunks_exact(width * height)
         .map(Into::into)
@@ -21,5 +39,10 @@ mod test {
                 vec![b'7', b'8', b'9', b'0', b'1', b'2']
             ]
         );
+    }
+
+    #[test]
+    fn main() {
+        super::do_main(std::fs::File::open("../inputs/day_08.txt").unwrap());
     }
 }
